a8f95336ca547b608942eff8072985275495f3fe
moritamu <moris@r.sannet.ne.jp>
2021-01-26 23:38:20 +0900

    Initial Commit

---
+++ Tanjyou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tanjyou
+//
+//  Created by 森田晋 on 2021/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tanjyou/TanjyouApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanjyouApp.swift
+//  Tanjyou
+//
+//  Created by 森田晋 on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TanjyouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
